a42fe9ac4c8239c1eb37576a932422f25345dc37
Tobi Lasisi <tobi.lasisi@gmail.com>
2021-08-16 01:51:40 +0000

    Update message for when test is failed.
    Signed-off-by: Tobi Lasisi <tobi.lasisi@gmail.com>

--- frontend/src/app/questions/user-answers/user-answers.component.ts
+++ frontend/src/app/questions/user-answers/user-answers.component.ts
@@ -95,7 +95,7 @@ export class UserAnswersComponent implements OnInit {
           if (this.passedCutoff) {
             this.alertService.success(`You met the criteria for online classes. If you are interested, please fill out the contact form below. We will be in touch`);
           } else {
-            this.alertService.error(`You are not eligible for online classes. Please contact MASEP at ${this.adminPhonenumber} to schedule your in-person class.`);
+            this.alertService.error(`You are ineligible to take MASEP Online. If you are in the state of Mississippi, please see your court order for further instruction on where to attend MASEP in person. If you are out of state, please contact MASEP at 662-325-3423`);
           }
           this.loading = false;
           this.email = '';
