22bd265e1c10673d6fe0655c1881b4e4f5714979
Tobi Lasisi <tobi.lasisi@gmail.com>
2022-01-10 01:35:20 +0100

    make uploads path configurable
    Signed-off-by: Tobi Lasisi <tobi.lasisi@gmail.com>

--- routes/answersRoutes.js
+++ routes/answersRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const props = require("../config");
 const answerController = require('../controllers/answer.controller');
 
 const router = express.Router();
 
 const  multipart  =  require('connect-multiparty');
-const  multipartMiddleware  =  multipart({ uploadDir:  './uploads' });
+const  multipartMiddleware  =  multipart({ uploadDir:  props.uploadsPath });
 
 router.get('/checkAvailability/:email', answerController.checkAvailability);
 router.post('/submit', answerController.submitAnswers);
